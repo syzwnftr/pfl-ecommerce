468b776ea7dfce656f35d2f685a6086fd301cc32
Syazwan Fitri <syazfitry69@gmail.com>
2022-09-21 06:33:23 +0800

    Create Context folder, StateContext.js

---
+++ context/StateContext.js
@@ -0,0 +1,22 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { toast } from "react-hot-toast";
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+    const [showCart, setShowCart] = useState(false);
+    const [cartItem, setCartItem] = useState();
+    const [totalPrice, setTotalPrice] = useState();
+    const [totalQuantities, setTotalQuantities] = useState();
+    const [qty, setQty] = useState(1);
+
+    return (
+        <Context.Provider 
+            value={{
+                showCart, cartItem, totalPrice, totalQuantities, qty
+            }}
+        >
+            {children}
+        </Context.Provider>
+    )
+}
