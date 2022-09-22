e605d17c0497898afad7c780261c7bccf5bc43cd
Syazwan Fitri <syazfitry69@gmail.com>
2022-09-22 17:09:45 +0800

    Added Toaster

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import { Layout } from '../components';
+import { Toaster } from 'react-hot-toast';
 import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
       <Layout>
+        <Toaster />
         <Component {...pageProps} />
       </Layout>
     </StateContext>
