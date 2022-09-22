02f7925fb849181e1c6bb419a1a06d4174599c9c
Syazwan Fitri <syazfitry69@gmail.com>
2022-09-22 17:09:13 +0800

    Added function for - and + button in product details

--- context/StateContext.js
+++ context/StateContext.js
@@ -10,13 +10,32 @@ export const StateContext = ({ children }) => {
     const [totalQuantities, setTotalQuantities] = useState();
     const [qty, setQty] = useState(1);
 
+    const incQty = () => {
+        setQty((prevQty) => prevQty + 1)
+    }
+
+    const decQty = () => {
+        setQty((prevQty) => {
+            if(prevQty - 1 < 1) return 1;
+            return prevQty - 1;
+        });
+    }
+
     return (
         <Context.Provider 
             value={{
-                showCart, cartItem, totalPrice, totalQuantities, qty
+                showCart, 
+                cartItem, 
+                totalPrice, 
+                totalQuantities, 
+                qty,
+                incQty,
+                decQty
             }}
         >
             {children}
         </Context.Provider>
     )
 }
+
+export const useStateContext = () => useContext(Context);
