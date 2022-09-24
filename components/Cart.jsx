c3b14aa61c120154f23a2b8434a4c1c2461fdd0f
Syazwan Fitri <syazfitry69@gmail.com>
2022-09-24 21:39:56 +0800

    Added function to onClick

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -9,7 +9,7 @@ import { urlFor } from '../lib/client';
 
 const Cart = () => {
   const cartRef = useRef();
-  const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext();
+  const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity } = useStateContext();
 
   return (
     <div className='cart-wrapper' ref={cartRef}>
@@ -54,9 +54,19 @@ const Cart = () => {
                 <div className='flex bottom'>
                   <div>
                       <p className='quantity-desc'>
-                        <span className='minus' onClick=""><AiOutlineMinus /></span>
-                        <span className='num' onClick="">0</span>
-                        <span className='plus' onClick=""><AiOutlinePlus /></span>
+                        <span 
+                        className='minus' 
+                        onClick={() => toggleCartItemQuantity(item._id, 'dec')}
+                        > 
+                          <AiOutlineMinus />
+                        </span>
+                        <span className='num' onClick="">{item.quantity}</span>
+                        <span 
+                        className='plus' 
+                        onClick={() => toggleCartItemQuantity(item._id, 'inc')}
+                        >
+                          <AiOutlinePlus />
+                        </span>
                       </p>
                   </div>
                   <button 
