e84b3988b25ab213c208a875e0dbd06dce3d3c52
Syazwan Fitri <syazfitry69@gmail.com>
2022-09-24 22:07:05 +0800

    Created function to remove items from cart

--- context/StateContext.js
+++ context/StateContext.js
@@ -50,6 +50,18 @@ export const StateContext = ({ children }) => {
 
     };
 
+    const onRemove = (product) => {
+        foundProduct = cartItems.find((item) => item._id === product._id);
+
+        const newCartItems = cartItems.filter((item) => item._id !== product._id);
+
+        setTotalPrice(prevTotalPrice => prevTotalPrice - foundProduct.price * foundProduct.quantity);
+
+        setTotalQuantities(prevTotalQuantities => prevTotalQuantities - foundProduct.quantity);
+
+        setCartItems(newCartItems);
+    }
+
     const toggleCartItemQuantity = (id, value) => {
         foundProduct = cartItems.find((item) => item._id === id);
         index = cartItems.findIndex((product) => product._id === id);
@@ -85,7 +97,8 @@ export const StateContext = ({ children }) => {
                 incQty,
                 decQty,
                 onAdd,
-                toggleCartItemQuantity
+                toggleCartItemQuantity,
+                onRemove
             }}
         >
             {children}
